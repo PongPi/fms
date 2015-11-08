155430ce59e84d2185a2e743fc190f0012563668
Pong Pi <eroshaly@gmail.com>
2015-11-08 14:59:58 +0700

    Initial Source

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fmsApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
